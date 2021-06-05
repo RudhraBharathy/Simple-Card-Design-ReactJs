aacefcd80c711307f27e84239624335b5eed2e42
Rudhra Bharathy G <74898301+RudhraBharathy@users.noreply.github.com>
2021-06-05 18:12:33 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>BWD with Javascript And React.js</h1>
+      <p>
+        Hi ShapeAI,Myself Rudhra Bharathy this my third bootcamp in ShapeAI.I am
+        very happy to study in this platform with Shaurya Sinha sir.
+      </p>
+    </div>
+  );
+}
+export default Note;
